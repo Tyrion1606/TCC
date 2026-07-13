2124b4fb679147880d9445ba2ab99829cbe9143f
davi <davimonteiroalves16@gmail.com>
2026-07-13 18:05:06 -0300

    importando projeto do overleaf

---
+++ compila.bat
@@ -0,0 +1,13 @@
+pdflatex fcte_template
+bibtex fcte_template
+pdflatex fcte_template
+pdflatex fcte_template
+
+del "fcte_template.aux"
+del "fcte_template.bbl"
+del "fcte_template.blg"
+del "fcte_template.lof"
+del "fcte_template.log"
+del "fcte_template.lot"
+del "fcte_template.out"
+del "fcte_template.toc"
---
+++ compilation_files/my_hotkeys.vim
@@ -0,0 +1,3 @@
+map <f3> :wall<cr>:AsyncRun! compilation_files/compile<cr>
+map <f7> :wall<cr>:!compilation_files/compile bibtex<cr>
+map <s-f3> :wall<cr>:!compilation_files/compile<cr>
